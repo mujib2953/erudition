7ed516ad2c25defdfe0de6cb16574b3eeb29db01
mujib2953 <ansarimujiburrehman@gmail.com>
2019-12-19 19:19:47 +0530

    Days-004: Casting implemented

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository is created with intention to learn and evolve my Java skills to 
     - [Hello World](https://github.com/mujib2953/erudition/tree/master/src/com/learning/Days001)
     - [Expression](https://github.com/mujib2953/erudition/tree/master/src/com/learning/Days002)
     - [DataTypes](https://github.com/mujib2953/erudition/tree/master/src/com/learning/Days003)
-    
+    - [Casting](https://github.com/mujib2953/erudition/tree/master/src/com/learning/Days004)
     
 Author
 -
---
+++ src/com/learning/Days004/Casting.java
@@ -0,0 +1,19 @@
+package com.learning.Days004;
+
+public class Casting {
+    public static void main(String[] args) {
+
+        byte myMinByte = Byte.MIN_VALUE;
+        short myMinShort = Short.MIN_VALUE;
+        int myMinInt = Integer.MIN_VALUE;
+
+        int myInt = (myMinInt / 2); // --- does not throw any error
+
+        // byte myByte = (myMinByte / 2); // --- will throws an error, like require byte but getting int
+        byte myByte = (byte) (myMinByte / 2); // -- this is casting the output
+
+        // short myShort = (myMinShort / 2); // --- will throws an error, like require short but getting int
+        short myShort = (short) (myMinShort / 2); // --this casting
+
+    }
+}
