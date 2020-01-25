7b4e3dc4818f94fdb109697d1148a3bef853ebd7
mujib2953 <ansarimujiburrehman@gmail.com>
2020-01-25 11:54:45 +0530

    Days-061: Cuboid Challenge Completed.

---
+++ src/com/learning/days061to080/days061/Cuboid.java
@@ -0,0 +1,19 @@
+package com.learning.days061to080.days061;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return super.getArea() * this.height;
+    }
+}
